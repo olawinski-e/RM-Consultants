12784927455962bf92b1beec40f38116dc0ab5b4
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2019-11-22 03:56:06 +0100

    header ok / navbar ok / collapse menu ok

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+// * STICKY HEADER *
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
