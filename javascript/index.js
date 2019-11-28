023f0280817d79d8d7b3a79f7aa53426459fb047
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2019-11-28 13:49:39 +0100

    hard header

--- javascript/index.js
+++ javascript/index.js
@@ -21,3 +21,13 @@ $(window).on("load", function() {
     }
   }
 });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function dropMenu() {
+  var x = document.getElementById("navbarSupportedContent");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
